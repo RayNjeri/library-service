0862d44ab58e34685b0d2fc1a80465d15a21c2e2
Rachael Njeri <rachael.njeri@andela.com>
2018-10-06 13:48:25 +0300

    (ft): Handle book input validation
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- models/Books.js
+++ models/Books.js
@@ -1,36 +1,71 @@
 const mongoose = require('mongoose');
+const Joi = require('joi');
 const Schema = mongoose.Schema;
 
-const BooksSchema = new Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  author: {
-    type: String,
-    required: true
-  },
-  publicationYear: {
-    type: Number,
-    required: true
-  },
-  pages: {
-    type: Number
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  },
-  availability: {
-    type: Boolean,
-    default: true
-  },
-  borrowedDate: {
-    type: Date
-  },
-  dueDate: {
-    type: Date
-  }
-});
+const Book = mongoose.model(
+  'books',
+  new Schema({
+    title: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 150
+    },
+    author: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 50
+    },
+    publicationYear: {
+      type: Number,
+      required: true
+    },
+    pages: {
+      type: Number
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    },
+    availability: {
+      type: Boolean,
+      default: true
+    },
+    borrowedDate: {
+      type: Date
+    },
+    dueDate: {
+      type: Date
+    },
+    returnDate: {
+      type: Date
+    }
+  })
+);
 
-module.exports = Book = mongoose.model('books', BooksSchema);
+function bookValidation(bookDescription) {
+  const schema = {
+    title: Joi.string()
+      .max(150)
+      .min(5)
+      .required(),
+    author: Joi.string()
+      .max(50)
+      .min(5)
+      .required(),
+    publicationYear: Joi.number()
+      .required()
+      .greater(0)
+      .max(new Date().getFullYear()),
+    pages: Joi.number()
+      .required()
+      .greater(0),
+    borrowedDate: Joi.date().max(new Date()),
+    returnDate: Joi.date().max(new Date())
+  };
+
+  return Joi.validate(bookDescription, schema);
+}
+exports.Book = Book;
+exports.validate = bookValidation;
--- routes/api/books.js
+++ routes/api/books.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-const Book = require('../../models/Books');
+const { Book, validate } = require('../../models/Books');
 
 //create a new book
 router.post('/register', (req, res) => {
+  // Input validation
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Find if a book with that title already exists.
   Book.findOne({ title: req.body.title }).then(book => {
     if (book) {
       return res
         .status(400)
         .json({ message: 'A book with that title already exist' });
-    } else {
+    }
+    // else create a new book
+    else {
       const newBook = new Book({
         title: req.body.title,
         author: req.body.author,
