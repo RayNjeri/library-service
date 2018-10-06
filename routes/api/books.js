ef8454a95ec6bce121b1bfb92eca08c690aa0601
Rachael Njeri <rachael.njeri@andela.com>
2018-10-06 16:46:41 +0300

    (ft): PUT - Update book fields
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- routes/api/books.js
+++ routes/api/books.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { Book, validate } = require('../../models/Books');
+const validateId = require('../../utils/validateObjectId');
 
 //create a new book
 router.post('/register', (req, res) => {
@@ -14,7 +15,7 @@ router.post('/register', (req, res) => {
     if (book) {
       return res
         .status(400)
-        .json({ message: 'A book with that title already exist' });
+        .json({ message: 'A book with that title already exist.' });
     }
     // else create a new book
     else {
@@ -37,7 +38,7 @@ router.get('/:id', (req, res) => {
   Book.findById(req.params.id)
     .then(book => res.json(book))
     .catch(err =>
-      res.status(404).json({ message: 'No book found with that Id' })
+      res.status(404).json({ message: 'No book found with that Id.' })
     );
 });
 
@@ -45,11 +46,31 @@ router.get('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
   Book.findById(req.params.id)
     .then(book => {
-      book.remove().then(() => res.json({ message: 'Successfully deleted' }));
+      book.remove().then(() => res.json({ message: 'Successfully deleted.' }));
     })
     .catch(err =>
-      res.status(404).json({ message: 'No book found with that Id' })
+      res.status(404).json({ message: 'No book found with that Id.' })
     );
+});
+
+//Update book
+router.put('/:id', validateId, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let book = await Book.findById(req.params.id);
+  if (!book)
+    return res.status(404).json({ message: 'No book found with that Id.' });
+
+  const fields = {
+    publicationYear: req.body.publicationYear || book.publicationYear,
+    pages: req.body.pages || book.pages
+  };
+  book = await Book.findOneAndUpdate({ _id: req.params.id }, fields, {
+    new: true
+  });
+
+  res.json(book);
 });
 
 module.exports = router;
